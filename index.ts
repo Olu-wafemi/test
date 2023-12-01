f3b6c75142607e0aad1d113ae280898624cabd2c
Oluwafemi <femio82@gmail.com>
2023-12-01 19:09:06 +0100

    yws

--- index.ts
+++ index.ts
@@ -37,6 +37,15 @@ app.use((req, res, next) => {
   next()
 });
 
+app.use(function (req, res, next) {
+
+  res.setHeader('Access-Control-Allow-Origin', "*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type,set-cookie');
+  res.setHeader('Access-Control-Allow-Credentials', "true");
+  next();
+});
+
 
 export const receiverIdToSocketIdMap: any = {};
 
